b57a4b4fc02667e9e1f05d1ee51f82af3790ebe7
Paul Williams <paul@paulswilliams.me.uk>
2018-05-23 18:52:01 +0100

    Return user object from api

--- src/main/java/simple/auth/AuthClient.java
+++ src/main/java/simple/auth/AuthClient.java
@@ -1,11 +1,23 @@
 package simple.auth;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class AuthClient {
+    private static final Logger log = LoggerFactory.getLogger(AuthClient.class);
+
     public User login(String username, String password) {
-        return null;
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("http://localhost:8081/login")
+                .queryParam("username", username)
+                .queryParam("password", password);
+
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(builder.build().toUri(), User.class);
     }
 }
