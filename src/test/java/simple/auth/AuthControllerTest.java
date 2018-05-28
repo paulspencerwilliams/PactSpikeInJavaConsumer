4bc1cde9f271725cffc8662fba257df0e99192fd
Paul Williams <paul@paulswilliams.me.uk>
2018-05-28 20:23:30 +0100

    Partial validation

--- src/test/java/simple/auth/AuthControllerTest.java
+++ src/test/java/simple/auth/AuthControllerTest.java
@@ -11,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
@@ -44,5 +47,82 @@ public class AuthControllerTest {
         verify(authClient).login("paul", "secret");
         assertThat(homepage.isHtmlPage(), is(true));
         assertThat(((HtmlPage)homepage).getBody().querySelector("p").getTextContent(), is("Hi, Paul Williams"));
+    }
+
+    @Test
+    public void registrationFormUsernameTooShort() throws IOException {
+        Page page = wc.getPage("/registrationForm");
+        assertThat(page.isHtmlPage(), is(true));
+
+        HtmlElement body = ((HtmlPage)page).getBody();
+        body.<HtmlTextInput>querySelector("#txtUsername").setText("abc");
+
+        Page returnedForm = body.<HtmlSubmitInput>querySelector("#btnRegister").click();
+        assertThat(returnedForm.isHtmlPage(), is(true));
+        assertThat(((HtmlPage) returnedForm).querySelector("#errorUsername").getTextContent(), containsString("size must be between 4 and 30"));
+    }
+
+    @Test
+    public void registrationFormUsernameTooLong() throws IOException {
+        Page page = wc.getPage("/registrationForm");
+        assertThat(page.isHtmlPage(), is(true));
+
+        HtmlElement body = ((HtmlPage)page).getBody();
+        body.<HtmlTextInput>querySelector("#txtUsername").setText("abcdefghijklmnopqrstuvwxyzabcde");
+
+        Page returnedForm = body.<HtmlSubmitInput>querySelector("#btnRegister").click();
+        assertThat(returnedForm.isHtmlPage(), is(true));
+        assertThat(((HtmlPage) returnedForm).querySelector("#errorUsername").getTextContent(), containsString("size must be between 4 and 30"));
+    }
+
+    @Test
+    public void registrationFormAgeEmpty() throws IOException {
+        Page page = wc.getPage("/registrationForm");
+        assertThat(page.isHtmlPage(), is(true));
+
+        HtmlElement body = ((HtmlPage)page).getBody();
+
+        Page returnedForm = body.<HtmlSubmitInput>querySelector("#btnRegister").click();
+        assertThat(returnedForm.isHtmlPage(), is(true));
+        assertThat(((HtmlPage) returnedForm).querySelector("#errorAge").getTextContent(), containsString("must not be null"));
+    }
+
+    @Test
+    public void registrationFormAgeContainsAlpha() throws IOException {
+        Page page = wc.getPage("/registrationForm");
+        assertThat(page.isHtmlPage(), is(true));
+
+        HtmlElement body = ((HtmlPage)page).getBody();
+        body.<HtmlTextInput>querySelector("#txtAge").setText("a");
+
+        Page returnedForm = body.<HtmlSubmitInput>querySelector("#btnRegister").click();
+        assertThat(returnedForm.isHtmlPage(), is(true));
+        assertThat(((HtmlPage) returnedForm).querySelector("#errorAge").getTextContent(), containsString("must not be null"));
+    }
+
+    @Test
+    public void registrationFormPasswordTooShort() throws IOException {
+        Page page = wc.getPage("/registrationForm");
+        assertThat(page.isHtmlPage(), is(true));
+
+        HtmlElement body = ((HtmlPage)page).getBody();
+        body.<HtmlPasswordInput>querySelector("#txtPassword").setText("abcde");
+
+        Page returnedForm = body.<HtmlSubmitInput>querySelector("#btnRegister").click();
+        assertThat(returnedForm.isHtmlPage(), is(true));
+        assertThat(((HtmlPage) returnedForm).querySelector("#errorPassword").getTextContent(), containsString("size must be between 6 and 128"));
+    }
+
+    @Test
+    public void registrationFormPasswordTooLong() throws IOException {
+        Page page = wc.getPage("/registrationForm");
+        assertThat(page.isHtmlPage(), is(true));
+
+        HtmlElement body = ((HtmlPage)page).getBody();
+        body.<HtmlPasswordInput>querySelector("#txtPassword").setText("lskjfsdfsfsdffsdf");
+
+        Page returnedForm = body.<HtmlSubmitInput>querySelector("#btnRegister").click();
+        assertThat(returnedForm.isHtmlPage(), is(true));
+        assertThat(((HtmlPage) returnedForm).querySelector("#errorPassword").getTextContent(), containsString("size must be between 6 and 128"));
     }
 }
